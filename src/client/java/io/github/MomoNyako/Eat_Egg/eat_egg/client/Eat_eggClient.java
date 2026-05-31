e126b4fdf1ace1a48f91254293ad69593b0ff9bb
MomoNyako <2057567119@qq.com>
2026-05-31 14:54:22 +0800

    创建了Egg Eat仓库

---
+++ src/client/java/io/github/MomoNyako/Eat_Egg/eat_egg/client/Eat_eggClient.java
@@ -0,0 +1,10 @@
+package io.github.MomoNyako.Eat_Egg.eat_egg.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Eat_eggClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/MomoNyako/Eat_Egg/eat_egg/Eat_egg.java
@@ -0,0 +1,10 @@
+package io.github.MomoNyako.Eat_Egg.eat_egg;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Eat_egg implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
